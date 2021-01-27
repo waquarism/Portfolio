bc3810bbba701ee487d6f6026e546d4f385bd443
Arsh Sahzad <admin@arsh.dev>
2021-01-27 22:11:25 +0530

    info update

--- src/dataSets/dataSet/dataSet.js
+++ src/dataSets/dataSet/dataSet.js
@@ -14,13 +14,13 @@ export const Person = {
 export const AboutMeData = {
     intro: 'I\'m Full Stack Web Developer and a Tech Enthusiast who loves everything which involves code.',
     desc : 'I enjoy taking up complex problems and converting to an usable format. My major domains are Web Development and Python Programming.',
-    outro: 'When I\'m not coding, you will find me watching an episode of The Office or exploring a new place.'
+    outro: 'When I\'m not coding, you will find me watching an episode of The Office or exploring for new tech & technologies.'
 };
 
 export const Experiences = [
     {
         position : 'Web Developer',
-        organization : 'CodingExpo',
+        organization : 'CodingExpo Technology Private Limited',
         startDate : 'January 2020',
         endDate : 'Present',
         description : 'Responsible for designing, coding and modifying web apps according to client’s needs & fixes bugs in existing projects.',
@@ -28,18 +28,18 @@ export const Experiences = [
     },
     {
         position : 'Web Developer ~ Intern',
-        organization : 'CodingExpo',
+        organization : 'CodingExpo Technology Private Limited',
         startDate : 'October 2019',
         endDate : 'December 2019',
-        description : 'Worked on a LMS platform & responsible for front-end part, database management & payment gateway integration to web app',
+        description : 'Worked on a LMS platform & responsible for front-end part, database management & payment gateway integration to web app.',
         organizationPicture : CodingExpo
     },
     {
         position : 'Web Designer ~ Intern',
-        organization : 'SoftRetic',
+        organization : 'SoftRetic Private Limited',
         startDate : 'February 2019',
         endDate : 'April 2019',
-        description : 'Designed website, mobile application UI and UX, logo, cover photo, banners using figma, sketch, photoshop and illustrator',
+        description : 'Designed website, mobile application UI and UX, logo, cover photo, banners using figma, sketch, photoshop and illustrator.',
         organizationPicture : SoftRetic
     }
 ];
@@ -47,20 +47,20 @@ export const Experiences = [
 export const EducationList = [
     {
         instituteName : 'Greater Noida Institute of Technology',
-        degree : 'B.Tech – Information Technology (IT)',
-        year : '2021',
+        degree : 'B.Tech – Information Technology (IT) ',
+        year : ' 2017-2021',
         id : 1
     },
     {
         instituteName : 'Mira Mateshwari M.S.S School',
         degree : 'Senior Secondary ',
-        year : '2016',
+        year : ' 2014-2016',
         id : 2
     },
     {
         instituteName : 'DAV Centenary Public School',
         degree : 'Secondary School ',
-        year : '2014',
+        year : ' 2004-2014',
         id : 3
     }
 ];
@@ -68,25 +68,25 @@ export const EducationList = [
 export const Projects = [
     {
         projectName : 'MixFusion',
-        description : 'Publishing a python open source library & it is a simple remixing tool for python which can create fusion of two or more songs change its tempo, frequency etc',
+        description : 'Publishing a python open source library & it is a simple remixing tool for python which can create fusion of two or more songs change its tempo, frequency etc.',
         technologyStack : ['Python','Django','npm'],
         link : 'https://arsh.link/mixfusion'
     },
     {
         projectName : 'Windfolio',
-        description : 'It is a web application portfolio based on theme of Windows 10. It replicates the feature of the OS and it uses technology such as node.js and react.js',
+        description : 'It is a web application portfolio based on theme of Windows 10. It replicates the feature of the OS and it uses technology such as node.js and react.js.',
         technologyStack : ['HTML','CSS','JavaScript','Node.js','React.js'],
         link : 'https://arsh.link/windfolio'
     },
     {
         projectName : 'GitWatcher',
-        description : 'A github profile analyze platform where we can get the information of github user like total public repos, forks. It fetches data using Github API & axios library for requests',
+        description : 'A github profile analyze platform where we can get the information of github user like total public repos, forks. It fetches data using Github API & axios library for requests.',
         technologyStack : ['React.js','Node.js','Axios','Github API'],
         link : 'https://arsh.link/gitwatcher'
     },
     {
         projectName : 'CalmSound',
-        description : 'Sound healing therapy uses aspects of nature\'s sounds to improve physical and emotional health. It is based node.js, react.js, and pre-recorded sounds of nature',
+        description : 'Sound healing therapy uses aspects of nature\'s sounds to improve physical and emotional health. It is based node.js, react.js, and pre-recorded sounds of nature.',
         technologyStack : ['HTML','CSS','JavaScript','Node.js','Dockerfile'],
         link : 'https://arsh.link/calmsound'
     }, 
@@ -97,7 +97,7 @@ export const SkillSet = [
         name : 'Language Proficiency',
         values : [
             'HTML',
-            'CSS'
+            'CSS',
             'JavaScript',
             'ReactJS',
             'Python',
@@ -109,7 +109,7 @@ export const SkillSet = [
             'Git',
             'Github',
             'Docker',
-            'Kubernetes'
+            'Kubernetes',
             'PyCharm',
             'WebStrom',
             'Linux OS',
@@ -121,7 +121,7 @@ export const SkillSet = [
         values : [
             'MySQL',
             'MongoDB',
-            'Firestore'
+            'Firestore',
         ]
     }
 ]
@@ -174,5 +174,3 @@ export const LocationDetails = [
         dy: -90 
     }
 ]
-
-
